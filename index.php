6ac9f282d385bea825f15725304a6847271e87c0
buloshka <dmitri.ikimenco@gmail.com>
2022-12-21 12:57:59 +0300

    2

--- index.php
+++ index.php
@@ -1,6 +1,55 @@
 <?php
+function formating($value)
+{
+    $value = ceil($value);
+    return (string)number_format($value,0,'',' ')." ₽";
+}
+
 $is_auth = rand(0, 1);
 $user_name = 'Дмитрий'; // укажите здесь ваше имя
+
+$category = ["Доски и лыжи", "Крепления",
+             "Ботинки", "Одежда",
+             "Инструменты", "Разное"];
+$product = array(
+    array(
+        "name" => "2014 Rossignol District Snowboard",
+        "category" => "Доски и лыжи",
+        "price" => 10999,
+        "URL" => "img/lot-1.jpg"
+    ),
+    array(
+        "name" => "DC Ply Mens 2016/2017 Snowboard",
+        "category" => "Доски и лыжи",
+        "price" => 159999,
+        "URL" => "img/lot-2.jpg"
+    ),
+    array(
+        "name" => "Крепления Union Contact Pro 2015
+        года размер L/XL",
+        "category" => "Крепления",
+        "price" => 8000,
+        "URL" => "img/lot-3.jpg"
+    ),
+    array(
+        "name" => "Ботинки для сноуборда DC Mutiny Charocal",
+        "category" => "Ботинки",
+        "price" => 10999,
+        "URL" => "img/lot-4.jpg"
+    ),
+    array(
+        "name" => "Куртка для сноуборда DC Mutiny Charocal",
+        "category" => "Одежда",
+        "price" => 7500,
+        "URL" => "img/lot-4.jpg"
+    ),
+    array(
+        "name" => "Маска Oakley Canopy",
+        "category" => "Разное",
+        "price" => 5400,
+        "URL" => "img/lot-6.jpg"
+    )
+);
 ?>
 <!DOCTYPE html>
 <html lang="ru">
@@ -69,24 +118,28 @@ $user_name = 'Дмитрий'; // укажите здесь ваше имя
         </div>
         <ul class="lots__list">
             <!--заполните этот список из массива с товарами-->
-            <li class="lots__item lot">
-                <div class="lot__image">
-                    <img src="" width="350" height="260" alt="">
-                </div>
-                <div class="lot__info">
-                    <span class="lot__category">Название категории</span>
-                    <h3 class="lot__title"><a class="text-link" href="pages/lot.html">Название товара</a></h3>
-                    <div class="lot__state">
-                        <div class="lot__rate">
-                            <span class="lot__amount">Стартовая цена</span>
-                            <span class="lot__cost">цена<b class="rub">р</b></span>
-                        </div>
-                        <div class="lot__timer timer">
-                            12:23
+            <?php
+            foreach($product as &$value) {
+                echo '
+                <li class="lots__item lot">
+                    <div class="lot__image">
+                        <img src="',$value["URL"],'" width="350" height="260" alt="">
+                    </div>
+                    <div class="lot__info">
+                        <span class="lot__category">',$value["category"],'</span>
+                        <h3 class="lot__title"><a class="text-link" href="pages/lot.html">',$value["name"],'</a></h3>
+                        <div class="lot__state">
+                            <div class="lot__rate">
+                                <span class="lot__amount">Стартовая цена</span>
+                                <span class="lot__cost">',formating($value["price"]),'<b class="rub">р</b></span>
+                            </div>
+                            <div class="lot__timer timer">
+                                12:23
+                            </div>
                         </div>
                     </div>
-                </div>
-            </li>
+                </li>';
+            }?>
         </ul>
     </section>
 </main>
@@ -96,9 +149,12 @@ $user_name = 'Дмитрий'; // укажите здесь ваше имя
     <nav class="nav">
         <ul class="nav__list container">
             <!--заполните этот список из массива категорий-->
-            <li class="nav__item">
-                <a href="pages/all-lots.html">Название категории</a>
-            </li>
+            <?php foreach($category as &$value){
+                echo '
+                <li class="nav__item">
+                    <a href="pages/all-lots.html">',$value,'</a>
+                </li>';
+            }?>
         </ul>
     </nav>
     <div class="main-footer__bottom container">
